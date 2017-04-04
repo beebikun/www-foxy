a32ca2d201ba8f92e3f5adc1990cbb88bedd8552
kunla <beebikun@protonmail.ch>
2017-04-04 14:08:08 +0900

    split scripts

--- static/js/script.js
+++ static/js/script.js
@@ -21,78 +21,6 @@ function initRadioButtons(){
         }
     });
 }
-
-/*-----------------------------------------------*/
-
-
-/*--------------------Modal Section----------------*/
-var preModalFn;
-
-function showHideModal(el, show){
-    var classes = getClass(el);
-    if(show){
-        el.style.display = 'block';
-        setTimeout(function(){
-            addClass(el, 'in');
-            var div = document.createElement('div');
-            addClass(div, 'modal-backdrop fade in');
-            if(document.body != null) document.body.appendChild(div);
-        }, 50);
-    }else{
-        removeClass(el, 'in');
-        setTimeout(function(){
-            el.style.display = 'none'
-            removeEl('.modal-backdrop');
-        }, 50);
-    }
-    el.setAttribute('aria-hidden', show ? false : true)
-}
-
-
-
-function clickModalA(a){
-	//функция для onlick
-    if(preModalFn) preModalFn(a);
-    var id = a.getAttribute('href').slice(1);
-    var modal = document.getElementById( trim(id) );
-    if(modal) showHideModal(modal, true)
-}
-
-function initModal(){
-    var modalClass = '.modal';
-
-    //find and set all modal dismiss buttons
-    node2array(document.querySelectorAll('[data-dismiss=modal]')).forEach(
-        function(btn){
-            btn.onclick = function(e){
-                var modal = findParent(modalClass, this);
-                if(modal) showHideModal(modal);
-            }
-         }
-    );
-
-    //find and set all modal open elements (<a>)
-    node2array(document.querySelectorAll('[data-toggle=modal]')).forEach(
-        function(a){
-            a.onclick = function(){clickModalA(this);}
-        }
-    );
-
-    //find and set all modal elements (<div class="modal">)
-    node2array(document.querySelectorAll(modalClass)).forEach(
-        function(modal){
-            modal.onclick = function(e){
-                if( ~e.target.className.split(' ').indexOf('modal') ) showHideModal(this)}
-        }
-    );
-}
-/*-----------------------------------------------*/
-
-/*----------------Carusel Section---------------*/
-
-
-/*-----------------------------------------------*/
-
 /*------------------Map Section-----------------*/
 
 function initMap(){
@@ -120,15 +48,16 @@ function initMap(){
 window.onload = function(){
     initMap();
     initModal();
+    initAddresses();
 
     tagnameEach( 'table',  function(el){ addClass(el, "table table-bordered") } );
     tagnameEach( 'img', function(el){ addClass(el, "img-responsive") });
-    tagnameEach( 'input', function(el){ el.setAttribute('autocomplete','off') }); //Если js отключен - то пусть хоть такая подсказка остается
-    /*node2array( document.getElementsByClassName('map') ).forEach(function(el){ showHideElBlock(el, true) });*/
+    // Если js отключен - то пусть хоть такая подсказка остается
+    tagnameEach( 'input', function(el){ el.setAttribute('autocomplete','off') });
 
 
     (function(){
-        //Open/close menu for mobile divices
+        // Open/close menu for mobile divices
         var body = document.getElementsByTagName('body')[0];
         document.getElementById('collapseMenuBtn').onclick = function(e){
             //window.scrollTo(0,0);
@@ -138,86 +67,13 @@ window.onload = function(){
 
 
     (function(){
-        //Hightigh a current content-menu item
+        // Highlight a current content-menu item
         var contentmenu = document.getElementsByClassName('content-menu');
         var contentmenuEl = contentmenu.length ? node2array(contentmenu[0].getElementsByTagName('a')) : [];
         var path = window.location.pathname;
         for (var i = contentmenuEl.length - 1; i >= 0; i--) {
             var a = contentmenuEl[i];
             if(a.getAttribute('href')==path) addClass(a, 'active');
-        }
-    })();
-
-
-    (function() {
-        //Behavior in address building num input
-        if(!numInput) return
-        numInput.onkeydown = function(e){
-            var code = e.keyCode || e.which;
-            if(code==27){//esc
-                this.blur();
-                return
-            }
-            if(code==13){//enter
-                return
-            }
-        }
-    })();
-
-
-    (function(){
-        //Behavior in address street name input
-        if(!streetInput) return
-        var ul = streetTips.getElementsByTagName('ul')[0];
-
-        streetInput.onblur = function(){
-            setTimeout(function () {showHideElBlock(streetTips); }, 100)
-        }
-
-        streetInput.onkeydown = function(e){
-            var code = e.keyCode || e.which;
-            if(code==27){//esc
-                this.blur();
-                return
-            }
-            if(code==13){//enter
-                var liActive = node2array(ul.getElementsByClassName('active'));
-                if(liActive.length) streetInput.value = liActive[0].innerText || liActive[0].innerHTML;
-                return
-            }
-            if(code == 40 || code == 38){//40 - arrow down; 38 - arrow up
-                (function(isUp){
-                    var lis = node2array(ul.getElementsByTagName('li'));
-                    var liActive = lis.filter(function(li){return hasClass(li, 'active')});
-                    var liNext = (function(){
-                        var fn = isUp ? 'previousElementSibling' : 'nextElementSibling';
-                        return liActive.length ? liActive[0][fn] : null;;
-                    })();
-                    for (var i = liActive.length - 1; i >= 0; i--) {
-                        removeClass(liActive[i], 'active');
-                    };
-                    addClass(liNext ? liNext : lis[0], 'active');
-                })(code == 38 ? true : false);
-            } else{
-                var newval = streetInput.value;
-                if(newval.length < 3) return;
-                ul.innerHTML = '';
-                request({
-                    path: 'street',
-                    query: {name: newval},
-                    success: function(data){
-                        var streetsVal = 2, streetLen = data.length;
-                        if(streetLen==0) return;
-                        for (var i = streetLen > streetsVal ? streetsVal : (streetLen - 1); i >= 0; i--) {
-                            var el = document.createElement('li'), name = data[i].name;
-                            el.innerHTML = name;
-                            el.onclick = function(){streetInput.value = name;}
-                            ul.appendChild(el);
-                        };
-                        showHideElBlock(streetTips, true);
-                    }
-                })
-            }
         }
     })();
 
