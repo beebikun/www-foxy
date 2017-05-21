ed19c8a117108f528254a257ddb8b48dd1c1b3a6
kunla <beebikun@protonmail.ch>
2017-05-21 18:01:32 +0900

    use vars for tvchan counts

--- tlvx/views/rates.py
+++ tlvx/views/rates.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.views.generic import TemplateView
 
-from tlvx.core.models import Rates
+from tlvx.core.models import Rates, StaticPage
 
 
 class RatesView(TemplateView):
@@ -10,6 +10,8 @@ class RatesView(TemplateView):
     def get(self, response, name='p'):
         context = {
             'result': Rates.objects.filter(rtype__name=name).order_by('-date_in'),
+            'tv_count': StaticPage.objects.get(name='tv_count').display_name,
+            'televoxtv_tv_count': StaticPage.objects.get(name='televoxtv_tv_count').display_name,
         }
         return self.render_to_response(context)
 
